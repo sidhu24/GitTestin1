6c74ce44b796c1df811bcf7920c8cd2be51d7218
sidhu24 <kodandapani524@gmail.com>
2016-12-01 09:06:29 +0530

    new rebase test

--- GitTesting2/src/com/sone/test/Test.java
+++ GitTesting2/src/com/sone/test/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		System.out.println("hello world how are you.........");
+		System.out.println("hello world how are you.........");
+		System.out.println("hello world how are you.........");
 	}
 
 }
